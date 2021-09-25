682da7a62319b22e470fe06eddb1be10f93a697a
trojan782 <davidjacobs522@gmail.com>
2021-09-26 00:34:05 +0100

    binded the task repo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repositories\TaskRepository;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\TaskRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(TaskRepositoryInterface, TaskRepository);
     }
 
     /**
