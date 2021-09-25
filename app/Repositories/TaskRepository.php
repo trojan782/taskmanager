aa5ca526e994832bb031151a8a763f3c6f716e0d
trojan782 <davidjacobs522@gmail.com>
2021-09-26 00:48:20 +0100

    finished the task repository

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -6,13 +6,40 @@ use App\Models\Task;
 class TaskRepository implements TaskRepositoryInterface
 {
 
-    protected $model;
-    public function __construct(Task $Task)
+    protected $task;
+    public function __construct(Task $task)
     {
-        $this->model = $Task;
+        $this->task = $task;
     }
+
+    public function store($data)
+    {
+        $task = $this->task::create($data);
+        return $task;
+    }
+
+    public function show($id)
+    {
+        $task = $this->task::findOrFail($id);
+        return $task;
+    }
+
     public function all()
     {
-        return Task::all();
+        $tasks = $this->task::all();
+        return $tasks;
+    }
+
+    public function update($data, $id)
+    {
+        $task = $this->task::findOrFail($id);
+        $task->update($data);
+        return $task;
+    }
+
+    public function destroy($id)
+    {
+        $task = $this->task::findOrFail($id);
+        return $task->destroy();
     }
 }
