e16ff2d8d6e652b1391ef77d413978323cdeb7c7
trojan782 <davidjacobs522@gmail.com>
2021-09-26 00:47:13 +0100

    finished the taskservice

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -5,5 +5,35 @@ use App\Repositories\TaskRepository;
 
 class TaskService extends TaskRepository
 {
-    
+    protected $taskservice;
+
+    public function __construct(TaskRepository $taskservice)
+    {
+        $this->taskservice = $taskservice;
+    }
+
+    public function store($data)
+    {
+       return  $this->taskservice->store($data);
+    }
+
+    public function show($id)
+    {
+        return $this->taskservice->show($id);
+    }
+
+    public function all()
+    {
+        return $this->taskservice->all();
+    }
+
+    public function update($data,$id)
+    {
+        return $this->taskservice->update($data, $id);
+    }
+
+    public function destroy($id)
+    {
+        return $this->taskservice->destroy($id);
+    }
 }
