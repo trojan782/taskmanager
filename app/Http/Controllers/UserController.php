a8ea06c2f7b7cc78d5b5c623628290b3d8e6899a
trojan782 <davidjacobs522@gmail.com>
2021-09-27 02:42:22 +0100

    implemented all methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -65,6 +66,49 @@ class UserController extends Controller
             'message' => 'Tokens Revoked',
             'status' => 'You have been successfully logged out'
         ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+        if(Auth::id() == $id)
+        {
+            $response = $this->userservice->update($validator, $id);
+
+            if(!response)
+            {
+                response()->json([
+                    'Message' => 'update successful!',
+                    'status' => 'success',
+                    'data' => $validator
+                ], 200);
+            }
+            return response()->json([
+                'Message' => 'Update failed!',
+                'status' => 'error'
+            ]);
+        }
+        return response()->json([
+            'Message' => 'Permission denied!'
+        ]);
+    }
+
+    public function profile(Request $request)
+    {
+       return auth()->user();
+    }
+
+    public function allUsers()
+    {
+        return $this->userservice->all();
+    }
+
+    public function show($id)
+    {
+        return $this->userservice->show($id);
     }
 
 }
