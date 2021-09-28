d2a405299144fa85f8c8b431d8a4418c1787216c
trojan782 <davidjacobs522@gmail.com>
2021-09-29 00:43:36 +0100

    added prefix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/tasks', [TaskController::Class, 'all']);
+
+
+Route::post('/register', [UserController::Class, 'register']);
+Route::post('/login', [UserController::Class, 'login']);
+
+//protected routes
+Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'taskmanager'], function () {
+
+    Route::get('/profile' , [UserController::class, 'profile']);
+    Route::get('/tasks', [TaskController::class, 'all']);
+    Route::post('/sign-out', [AuthenticationController::class, 'logout']);
+
+    Route::post('task/create', [TaskController::class, 'store']);
+});
+
