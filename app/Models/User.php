ddf3e5a102dfa5abf5b9d549e469f218aaaf2068
trojan782 <davidjacobs522@gmail.com>
2021-09-29 00:31:57 +0100

    modified the user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Task;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -25,7 +26,7 @@ class User extends Authenticatable
 
     public function tasks()
     {
-        return $this->hasMany(Task::Class, 'user_id', 'task_id');
+        return $this->hasMany('App\Models\Task', 'userId');
     }
     /**
      * The attributes that should be hidden for serialization.
